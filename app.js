d327cbe3e3d5b14a441270b7de23d50f54424d58
root <root@webby7718.vm>
2013-04-01 18:37:12 -0400

    use ejs views instead of jade, pass string in place of var to convert jade to html

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.locals.title = "Alex's Awesome Gallery";
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  app.set('view engine', 'ejs');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
